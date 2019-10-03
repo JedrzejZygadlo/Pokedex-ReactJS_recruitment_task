4e6e5272f33fb13e32a98ea807b4e2893fb743ff
JedrzejZygadlo <jedrekzygadlo@wp.pl>
2019-10-03 12:54:34 +0200

    Create PokemonList component

---
+++ src/components/PokemonList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class PokemonList extends React.Component{
+    render(){
+        return (
+            <div>
+                PokemonList
+            </div>
+        )
+    }
+}
+
+export default PokemonList;
