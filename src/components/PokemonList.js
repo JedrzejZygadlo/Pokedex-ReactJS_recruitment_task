6c0cebc737cadbf91f78007f37d3c25e9dc29a48
JedrzejZygadlo <jedrekzygadlo@wp.pl>
2019-10-03 15:53:06 +0200

    Fetch pokemons from API , save important data in store

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { fetchPokemons } from '../actions';
 
 class PokemonList extends React.Component{
+    componentDidMount(){
+        this.props.fetchPokemons();
+    }
     render(){
+        console.log(this.props.pokemons);
         return (
             <div>
                 PokemonList
@@ -10,5 +15,8 @@ class PokemonList extends React.Component{
         )
     }
 }
-
-export default PokemonList;
+const mapStateToProps = (state) => {
+    console.log(state);
+    return { pokemons: state.pokemons}
+}
+export default connect(mapStateToProps,{ fetchPokemons })(PokemonList);
--- src/reducers/pokemonReducer.js
+++ src/reducers/pokemonReducer.js
@@ -1,6 +1,17 @@
 export default (state = {}, action) => {
     switch(action.type) {
+        case 'FETCH_POKEMONS':
+            return action.payload.map(pokemon => {
+                return {
+                    ...state,
+                    id: pokemon.id,
+                    num: pokemon.num,
+                    name: pokemon.name,
+                    img: pokemon.img,
+                    type: pokemon.type
+                }
+            })
         default:
-        return state;
+            return state;
     }
 }
